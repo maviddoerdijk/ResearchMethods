afa174b44b8bab2f8fae55a99626e6eac7298ffe
david <davidmoerdijk@gmail.com>
2024-04-22 11:36:52 +0200

    init

---
+++ assignment1.R
@@ -0,0 +1,9 @@
+# You simulate a (large) number of datasets,
+# for which we know that the null-hypothesis is true. On each of these datasets you perform the
+# appropriate statistical test (for example an independent samples t-test), but you do so while
+# committing questionable research practices (QRPs). 
+
+
+
+# In the lecture of week 3, we have discussed
+# several of these QRPs and you can choose which ones you want to simulate.
---
+++ main.R
@@ -0,0 +1,10 @@
+set.seed(5)
+n <- 20
+x <- runif(n, 0, 10)
+e <- rnorm(n, 0, 12)
+y <- 12 + 3 * x + e
+
+plot(x, y)
+
+fit <- lm(y ~ x)
+summary(fit)
